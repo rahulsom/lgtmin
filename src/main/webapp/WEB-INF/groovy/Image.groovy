de4a23ee3ed951df648a64eb26ce9353c66b2575
Rahul Somasunderam <rahul.som@gmail.com>
2013-04-25 21:17:34 -0700

    Added methods to image

--- src/main/webapp/WEB-INF/groovy/Image.groovy
+++ src/main/webapp/WEB-INF/groovy/Image.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonBuilder
 import groovy.transform.ToString
 import groovyx.gaelyk.datastore.Entity
 import groovyx.gaelyk.datastore.Ignore
@@ -18,19 +19,40 @@ class Image {
   long dislikes = 0
   long credits = 100
 
-  @Ignore String getHash() {
-    def newId = (this.id as long) + 1000
-    Shortener.instance.encode(newId)
-  }
-
   static Image findByHash(String hash) {
     def theId = Shortener.instance.decode(hash) - 1000
     Image.get(theId)
   }
 
   static Image findByUrl(String url) {
-    Image.find {
-      where imageUrl == url
-    }
+    Image.find { where imageUrl == url }
+  }
+
+  @Ignore String getHash() {
+    def newId = (this.id as long) + 1000
+    Shortener.instance.encode(newId)
+  }
+
+  @Ignore String getDataUrl() {
+    "http://lgtm.in/i/${hash}"
+  }
+
+  @Ignore String getUpvoteUrl() {
+    "http://lgtm.in/u/${hash}"
+  }
+
+  @Ignore String getReportUrl() {
+    "http://lgtm.in/r/${hash}"
+  }
+
+  @Ignore String getMarkdown() {
+    "[![LGTM](${imageUrl})](${dataUrl})\n" +
+        "[:+1:](${upvoteUrl}) [:-1:](${reportUrl})"
+  }
+
+  @Ignore String toJson() {
+    def props = this.properties.findAll {k,v -> !(k in ['metaClass', '$key', 'class'])}
+    println props.keySet()
+    new JsonBuilder(props).toPrettyString()
   }
 }
