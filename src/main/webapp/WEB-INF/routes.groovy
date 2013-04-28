784abf8540cc0e15407b7348e4e78a258919a644
Rahul Somasunderam <rahul.som@gmail.com>
2013-04-27 20:12:24 -0700

    Added some caching, removed a plugin

--- src/main/webapp/WEB-INF/routes.groovy
+++ src/main/webapp/WEB-INF/routes.groovy
@@ -1,7 +1,7 @@
 
-get "/", forward: "/index.groovy"
+get "/", forward: "/index.groovy", cache: 5.minutes
 get "/g", forward: "/random.groovy"
-get "/g/upload", forward: "/upload.groovy"
+get "/g/upload", forward: "/upload.groovy", cache: 24.hours
 post "/g/save", forward: "/store.groovy"
-get "/i/@hash", forward: "/load.groovy?hash=@hash"
-get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png"
+get "/i/@hash", forward: "/load.groovy?hash=@hash", cache: 1.hours
+get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png", cache: 100.hours
