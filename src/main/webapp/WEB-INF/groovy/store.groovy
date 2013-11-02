5b74cdcfad2edd7c94e0f433e16c0d1882dd8453
Rahul Somasunderam <rahul.som@gmail.com>
2013-11-01 18:11:18 -0700

    Blocks non image submissions

--- src/main/webapp/WEB-INF/groovy/store.groovy
+++ src/main/webapp/WEB-INF/groovy/store.groovy
@@ -1,3 +1,9 @@
+import com.google.api.client.http.GenericUrl
+import com.google.api.client.http.HttpRequest
+import com.google.api.client.http.HttpRequestFactory
+import com.google.api.client.http.HttpRequestInitializer
+import com.google.api.client.http.HttpTransport
+import com.google.api.client.http.javanet.NetHttpTransport
 import domain.Image
 
 log.info "Setting attributes"
@@ -12,23 +18,34 @@ if (imageUrl.startsWith('http://') || imageUrl.startsWith('https://')) {
     request.setAttribute('message', 'That image was already uploaded.')
     redirect "/i/${existingImage.hash}"
   } else {
-    def newImage = new Image()
-    newImage.imageUrl = imageUrl
+    def HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
 
-    newImage.save()
+    HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
+          @Override
+          public void initialize(HttpRequest request) {
+          }
+        });
 
-    request.setAttribute 'image', newImage
-    request.setAttribute 'dataUrl', newImage.dataUrl
-
-    response.setHeader("Content-Type", "text/html");
-    redirect "/i/${newImage.hash}"
+    def ct = requestFactory.buildHeadRequest(new GenericUrl(imageUrl)).execute().contentType
+    if (ct.startsWith('image')) {
+      def newImage = new Image()
+      newImage.imageUrl = imageUrl
+      newImage.save()
+      request.setAttribute 'image', newImage
+      request.setAttribute 'dataUrl', newImage.dataUrl
+      response.setHeader("Content-Type", "text/html");
+      redirect "/i/${newImage.hash}"
+    } else {
+      request.setAttribute 'message', "Resource at specified url was not an image"
+      request.setAttribute 'imageUrl', imageUrl
+      response.setHeader "Content-Type", "text/html"
+      forward '/WEB-INF/pages/upload.gtpl'
+    }
   }
 } else {
-
   request.setAttribute 'message', "Only http and https urls are valid"
   request.setAttribute 'imageUrl', imageUrl
   response.setHeader "Content-Type", "text/html"
   forward '/WEB-INF/pages/upload.gtpl'
-
 }
 
