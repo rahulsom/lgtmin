ace6a70c994ac979cbaf28926aaa8d8c2a58844c
Rahul Somasunderam <rahul.som@gmail.com>
2014-01-22 20:33:30 -0800

    Fixed command for new gaelyk

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" = false ]; then
-  gradle -q clean test && gradle -q gaeUpload
+  gradle -q clean test && gradle -q gaeUpdateAll
 else
   gradle -q clean test
 fi
