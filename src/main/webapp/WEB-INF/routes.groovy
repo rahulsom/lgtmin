7852071c1687cc9263dd6ec67490ee1a979ae565
Rahul Somasunderam <rahul.som@gmail.com>
2014-01-22 21:00:51 -0800

    Removed caches

--- src/main/webapp/WEB-INF/routes.groovy
+++ src/main/webapp/WEB-INF/routes.groovy
@@ -1,8 +1,8 @@
 get "/", 						forward: "/index.groovy",											cache: 1.hours
 get "/g", 					forward: "/random.groovy"
-get "/g/upload",  	forward: "/upload.groovy",										cache: 1.hours
+get "/g/upload",  	forward: "/upload.groovy"
 post "/g/save", 		forward: "/store.groovy"
-get "/i/@hash", 		forward: "/load.groovy?hash=@hash",						cache: 1.hours
+get "/i/@hash", 		forward: "/load.groovy?hash=@hash"
 get "/u/@hash", 		forward: "/upvote.groovy?hash=@hash"
 get "/r/@hash", 		forward: "/report.groovy?hash=@hash"
 get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png", cache: 1.hours
