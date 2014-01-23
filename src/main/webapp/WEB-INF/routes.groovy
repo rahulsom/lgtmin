5ea5d4cb40419dbee3088672c933c98c546370e1
Rahul Somasunderam <rsom@certifydatasystems.com>
2014-01-22 16:40:14 -0800

    Reduced cache duration for pages

--- src/main/webapp/WEB-INF/routes.groovy
+++ src/main/webapp/WEB-INF/routes.groovy
@@ -1,9 +1,9 @@
-get "/", 						forward: "/index.groovy",											cache: 24.hours
+get "/", 						forward: "/index.groovy",											cache: 1.hours
 get "/g", 					forward: "/random.groovy"
-get "/g/upload",  	forward: "/upload.groovy",										cache: 24.hours
+get "/g/upload",  	forward: "/upload.groovy",										cache: 1.hours
 post "/g/save", 		forward: "/store.groovy"
-get "/i/@hash", 		forward: "/load.groovy?hash=@hash",						cache: 2400.hours
+get "/i/@hash", 		forward: "/load.groovy?hash=@hash",						cache: 1.hours
 get "/u/@hash", 		forward: "/upvote.groovy?hash=@hash"
 get "/r/@hash", 		forward: "/report.groovy?hash=@hash"
-get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png", cache: 2400.hours
-get "/robots.txt/", redirect: "/robots.txt", 											cache: 2400.hours
+get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png", cache: 1.hours
+get "/robots.txt/", redirect: "/robots.txt", 											cache: 1.hours
