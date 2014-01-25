738f8f23fdfe6c37b7f274ebfc871c70cff61cc3
Rahul Somasunderam <rahul.som@gmail.com>
2014-01-25 11:27:38 -0800

    Fixed url for revalidate

--- src/main/webapp/WEB-INF/routes.groovy
+++ src/main/webapp/WEB-INF/routes.groovy
@@ -7,4 +7,4 @@ get "/u/@hash", 		forward: "/upvote.groovy?hash=@hash"
 get "/r/@hash", 		forward: "/report.groovy?hash=@hash"
 get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png", cache: 1.hours
 get "/robots.txt/", redirect: "/robots.txt", 											cache: 1.hours
-get "/cron/revalidate",  forward: "/cron.revalidate.groovy"
+get "/cron/revalidate",  forward: "/cron/revalidate.groovy"
