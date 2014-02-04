02f227013c28c8dd84b10018d382fbf36da8d198
Rahul Somasunderam <rahul.som@gmail.com>
2014-02-03 19:55:52 -0800

    Added github secrets

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+mkdir src/main/resources/ || echo "Resources Dir exists"
+
+cat > src/main/resources/oauth.properties << EOF
+clientId=${LGTMIN_CLIENT_ID}
+clientSecret=${LGTM_SECRET}
+EOF
+
 if [ "$TRAVIS_PULL_REQUEST" = false ]; then
   gradle -q clean test && gradle -q gaeUpdateAll
 else
