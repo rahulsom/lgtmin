94f10a1708f345cf710be832a21cb047e606f059
Rahul Somasunderam <rsom@certifydatasystems.com>
2014-02-03 20:09:03 -0800

    Added login for upvote/report

--- src/main/webapp/WEB-INF/groovy/report.groovy
+++ src/main/webapp/WEB-INF/groovy/report.groovy
@@ -1,17 +1,24 @@
-import util.AppUtil
 import domain.Image
+import util.AppUtil
+import util.GithubAuthUtil
 
-log.info "Setting attributes"
+def githubAuthUtil = new GithubAuthUtil(request, response)
+
 String hash = params.hash
-def image = Image.findByHash(hash)
-log.info "Image: ${image}"
-if (image) {
-  datastore.withTransaction {
-    image.dislikes ++
-    image.updateCredits()
-    image.save()
-  }
-  redirect("/i/${hash}")
-} else {
-  redirect('/')
+if (hash) {
+    def image = Image.findByHash(hash)
+    log.info "Image: ${image}"
+    if (image) {
+        githubAuthUtil.withValidUser ("/r/${hash}") {
+            datastore.withTransaction {
+                image.dislikes++
+                image.updateCredits()
+                image.save()
+            }
+            AppUtil.instance.evictCache("/i/${hash}")
+            redirect("/i/${hash}")
+        }
+    } else {
+        redirect('/')
+    }
 }
--- src/main/webapp/WEB-INF/groovy/upvote.groovy
+++ src/main/webapp/WEB-INF/groovy/upvote.groovy
@@ -1,17 +1,24 @@
-import util.AppUtil
 import domain.Image
+import util.AppUtil
+import util.GithubAuthUtil
 
-log.info "Setting attributes"
+def githubAuthUtil = new GithubAuthUtil(request, response)
+
 String hash = params.hash
-def image = Image.findByHash(hash)
-log.info "Image: ${image}"
-if (image) {
-  datastore.withTransaction {
-    image.likes ++
-    image.updateCredits()
-    image.save()
-  }
-  redirect("/i/${hash}")
-} else {
-  redirect('/')
+if (hash) {
+    def image = Image.findByHash(hash)
+    log.info "Image: ${image}"
+    if (image) {
+        githubAuthUtil.withValidUser("/u/${hash}") {
+            datastore.withTransaction {
+                image.likes++
+                image.updateCredits()
+                image.save()
+            }
+        }
+        AppUtil.instance.evictCache("/i/${hash}")
+        redirect("/i/${hash}")
+    } else {
+        redirect('/')
+    }
 }
