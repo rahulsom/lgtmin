86d9ac98910eea330e6f1ee5adfa4dcbdbd929e4
Rahul Somasunderam <rahul.som@gmail.com>
2014-02-06 21:47:16 -0800

    Fixed test

--- src/functionalTest/groovy/SmokeSpec.groovy
+++ src/functionalTest/groovy/SmokeSpec.groovy
@@ -1,11 +1,11 @@
 import geb.spock.GebSpec
 
 class SmokeSpec extends GebSpec {
-    void "main page title should be 'Gaelyk'"() {
+    void "main page title should be 'LGTM.in/g'"() {
         when:
         go ''
 
         then:
-    title == 'LGTM.in/g'
+        title == 'LGTM.in/g'
     }
 }
