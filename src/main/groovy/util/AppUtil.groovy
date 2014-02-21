66cd0f5d55a6408a5dfbacdb7185bea21d23ce6c
Rahul Somasunderam <rsom@certifydatasystems.com>
2014-02-21 14:17:57 -0800

    Code cleanup

--- src/main/groovy/util/AppUtil.groovy
+++ src/main/groovy/util/AppUtil.groovy
@@ -22,6 +22,7 @@ class AppUtil {
     public static final int SECOND = 1000
     public static final int MINUTE = 60 * SECOND
     public static final int HOUR = 60 * MINUTE
+    public static final int DAY = 24 * HOUR
 
     /**
      * Application root
--- src/main/webapp/WEB-INF/groovy/picture.groovy
+++ src/main/webapp/WEB-INF/groovy/picture.groovy
@@ -4,7 +4,7 @@ import util.AppUtil
 import domain.Image
 
 String hash = params.hash
-Image image = AppUtil.instance.getCachedValue("/i/${hash}", new Expiration(24 * 60 * 60 * 1000, true)) {
+Image image = AppUtil.instance.getCachedValue("/i/${hash}", new Expiration(AppUtil.DAY, true)) {
   Image.findByHash(hash)
 }
 
