df023682e4a4d5f9bc1632199eb54cdb900c0222
Rahul Somasunderam <rahul.som@gmail.com>
2014-06-06 22:21:49 -0700

    Added firefox addon

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-mkdir src/main/resources/ || echo "Resources Dir exists"
-
 cat > src/main/resources/oauth.properties << EOF
 clientId=${LGTMIN_CLIENT_ID}
 clientSecret=${LGTM_SECRET}
