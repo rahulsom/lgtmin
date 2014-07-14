0ea1509455233041d14d98eaede0d723462cf707
Rahul Somasunderam <rahul.som@gmail.com>
2014-07-13 20:49:53 -0700

    Possible fix for build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ EOF
 
 if [ "$TRAVIS_PULL_REQUEST" = false ]; then
   if [ "$TRAVIS_BRANCH" = "master" ]; then
-    # gradle clean check && gradle appengineUpdateAll 
-    ./gradlew clean check && cat ./build/exploded-app/WEB-INF/appengine-generated/datastore-indexes-auto.xml
+    # gradle clean check && gradle appengineUpdateAll
+    ./gradlew clean test && ./gradlew appengineUpdateAll
   else
     gradle clean check
   fi
