463f9f83136a6911e2ee037bd52e4776785366c6
Rahul Somasunderam <rsom@certifydatasystems.com>
2016-02-02 12:57:27 -0800

    Print md5sum of oauth

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-mv .appcfg_oauth2_tokens_java $HOME/
-
 cat > src/main/resources/oauth.properties << EOF
 clientId=${LGTMIN_CLIENT_ID}
 clientSecret=${LGTM_SECRET}
@@ -11,6 +9,9 @@ EOF
 if [ "$TRAVIS_PULL_REQUEST" = false ]; then
   if [ "$TRAVIS_BRANCH" = "master" ]; then
     # gradle clean check && gradle appengineUpdateAll
+    mv .appcfg_oauth2_tokens_java $HOME/
+    md5sum ~/.appcfg_oauth2_tokens_java
+
     ./gradlew clean test && ./gradlew appengineUpdateAll
   else
     ./gradlew clean check
