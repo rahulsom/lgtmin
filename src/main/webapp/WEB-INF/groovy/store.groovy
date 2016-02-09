9f8f5986c7c05a38f3b0511089adf48b9dae99fb
Rahul Somasunderam <rahul.som@gmail.com>
2016-02-08 21:58:56 -0800

    Fix indents to 2 spaces

--- src/main/webapp/WEB-INF/groovy/store.groovy
+++ src/main/webapp/WEB-INF/groovy/store.groovy
@@ -11,30 +11,30 @@ log.info "Image Url saved will be ${imageUrl}"
 
 def githubAuthUtil = new GithubAuthUtil(request, response)
 if (githubAuthUtil.isAuthenticated()) {
-    def newImage = new Image(
-        imageUrl: imageUrl,
-        uploader: session.getAttribute(GithubAuthUtil.GITHUB_USERNAME),
-        uploaderEmail: session.getAttribute(GithubAuthUtil.GITHUB_EMAIL_PRIMARY)
-    )
-    try {
-        newImage.validate()
-        newImage.save()
-        request.setAttribute 'image', newImage
-        request.setAttribute 'dataUrl', newImage.dataUrl
-        response.setHeader "Content-Type", "text/html";
-        redirect "/i/${newImage.hash}"
-    } catch (UniqueConstraintViolatedException e) {
-        request.setAttribute 'message', 'That image was already uploaded.'
-        redirect "/i/${e.hash}"
-    } catch (ValidationException e) {
-        request.setAttribute 'message', e.message
-        request.setAttribute 'imageUrl', imageUrl
-        response.setHeader "Content-Type", "text/html"
-        forward '/WEB-INF/pages/upload.gtpl'
-    }
-} else {
-    request.setAttribute 'message', "You're not logged in yet!"
+  def newImage = new Image(
+      imageUrl: imageUrl,
+      uploader: session.getAttribute(GithubAuthUtil.GITHUB_USERNAME),
+      uploaderEmail: session.getAttribute(GithubAuthUtil.GITHUB_EMAIL_PRIMARY)
+  )
+  try {
+    newImage.validate()
+    newImage.save()
+    request.setAttribute 'image', newImage
+    request.setAttribute 'dataUrl', newImage.dataUrl
+    response.setHeader "Content-Type", "text/html";
+    redirect "/i/${newImage.hash}"
+  } catch (UniqueConstraintViolatedException e) {
+    request.setAttribute 'message', 'That image was already uploaded.'
+    redirect "/i/${e.hash}"
+  } catch (ValidationException e) {
+    request.setAttribute 'message', e.message
     request.setAttribute 'imageUrl', imageUrl
     response.setHeader "Content-Type", "text/html"
     forward '/WEB-INF/pages/upload.gtpl'
+  }
+} else {
+  request.setAttribute 'message', "You're not logged in yet!"
+  request.setAttribute 'imageUrl', imageUrl
+  response.setHeader "Content-Type", "text/html"
+  forward '/WEB-INF/pages/upload.gtpl'
 }
