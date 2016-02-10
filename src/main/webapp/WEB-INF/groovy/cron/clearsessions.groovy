66580b79b601b7ba3ad89a34a5e1e448d3cce7ae
Rahul Somasunderam <rahul.som@gmail.com>
2016-02-09 18:28:20 -0800

    Log keys being deleted

--- src/main/webapp/WEB-INF/groovy/cron/clearsessions.groovy
+++ src/main/webapp/WEB-INF/groovy/cron/clearsessions.groovy
@@ -1,22 +1,28 @@
 import com.google.appengine.api.datastore.FetchOptions
 import com.google.appengine.api.datastore.Query
 
+import static com.google.appengine.api.datastore.Query.SortDirection.ASCENDING
+
 log.info "Clearing sessions"
 def query = new Query('_ah_SESSION').
     setFilter(new Query.FilterPredicate('_expires', Query.FilterOperator.LESS_THAN, new Date().time)).
-    setKeysOnly()
+    setKeysOnly().
+    addSort('_expires', ASCENDING)
 
 def preparedQuery = datastore.prepare(query)
 
 int count = 0
+def keys = []
 try {
   preparedQuery.
       asIterable(FetchOptions.Builder.withLimit(1000)).
       each {
+        keys << it.key
         datastore.delete(it.key)
         count ++
       }
 } catch (Throwable e) {
   log.error "Could not delete", e
 }
+log.info "Keys: $keys"
 log.info "Done clearing $count sessions"
