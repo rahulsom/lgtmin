87b7a6c2542605a1d99e6f1381a2c208e39873b3
Rahul Somasunderam <rahul.som@gmail.com>
2016-10-01 21:04:32 -0700

    Make pruning efficient by filtering reads

--- src/main/webapp/WEB-INF/groovy/cron/revalidate.groovy
+++ src/main/webapp/WEB-INF/groovy/cron/revalidate.groovy
@@ -1,17 +1,24 @@
+import com.google.appengine.api.datastore.*
+import static com.google.appengine.api.datastore.FetchOptions.Builder.*
 import domain.Image
 import domain.ValidationException
 
 log.info("Starting cron.revalidate")
-Image.findAll().each { Image i ->
-    try {
+def q = new Query('Image').addFilter('isDeleted', Query.FilterOperator.EQUAL, false)
+PreparedQuery preparedQuery = datastore.prepare(q)
+
+preparedQuery.asList(withLimit(100)).
+    each { Entity entity ->
+      Image i = entity as Image
+      try {
         i.validate(false)
-    } catch (ValidationException e) {
+      } catch (ValidationException e) {
         log.info("Removing ${i}")
         i.isDeleted = true
         i.save()
-    } catch (Exception e) {
+      } catch (Exception e) {
         log.warning "Unexpected exception: $e for $i"
+      }
     }
-}
 memcache.clearAll()
 log.info("Done cron.revalidate")
