d975347b0bdcbf38bcb82b7f22e70a397b11e266
Rahul Somasunderam <rahul.som@gmail.com>
2017-08-18 17:46:42 -0700

    Improve performance for loading userlist

--- src/main/webapp/WEB-INF/groovy/userlist.groovy
+++ src/main/webapp/WEB-INF/groovy/userlist.groovy
@@ -4,7 +4,7 @@ import util.AppUtil
 
 String userName = params.username
 UserList myList = LgtmService.instance.getUserList(userName)
-def imageList = LgtmService.instance.imageList.findAll { img -> myList.hashes && myList.hashes.contains(img.hash) }
+def imageList = myList.hashes.collect { LgtmService.instance.getImage(it) }
 
 final int PAGESIZE = 32
 final int page = params.page ? Integer.parseInt(params.page) : 1
@@ -12,21 +12,21 @@ final int start = (page - 1) * PAGESIZE
 final int stop = Math.min(start + PAGESIZE - 1, imageList.size() - 1)
 log.info "Start: $start, Stop: $stop, ilS: ${imageList.size()}"
 if (start <= stop) {
-  request.setAttribute 'imageList', imageList[start..stop]
+    request.setAttribute 'imageList', imageList[start..stop]
 } else {
-  request.setAttribute 'imageList', []
+    request.setAttribute 'imageList', []
 }
 request.setAttribute 'username', userName
 request.setAttribute 'appUtil', AppUtil.instance
 if (stop + 1 < imageList.size()) {
-  request.setAttribute 'next', page + 1
+    request.setAttribute 'next', page + 1
 }
 if (start > 0) {
-  request.setAttribute 'prev', page - 1
+    request.setAttribute 'prev', page - 1
 }
 
 if (params.page != null) {
-  forward '/WEB-INF/pages/browse.gtpl'
+    forward '/WEB-INF/pages/browse.gtpl'
 } else {
-  forward '/WEB-INF/pages/browsePage.gtpl'
+    forward '/WEB-INF/pages/browsePage.gtpl'
 }
