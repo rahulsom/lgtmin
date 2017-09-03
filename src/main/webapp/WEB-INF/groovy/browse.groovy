3d4f63cb7c41d7980a9baf6e6b6ea2fb9de5bf60
Rahul Somasunderam <rahul.som@gmail.com>
2017-09-03 00:05:28 -0700

    Prevent exception when requesting high page number

--- src/main/webapp/WEB-INF/groovy/browse.groovy
+++ src/main/webapp/WEB-INF/groovy/browse.groovy
@@ -6,16 +6,21 @@ def PAGESIZE = 32
 def page = (params.page ? params.page as int : 1)
 def start = (page - 1) * PAGESIZE
 def stop = Math.min(start + PAGESIZE - 1, imageList.size() - 1)
-request.setAttribute 'imageList', imageList[start..stop]
-request.setAttribute 'appUtil', AppUtil.instance
-if (stop + 1 < imageList.size()) {
-    request.setAttribute 'next', page + 1
-}
-if (start > 0) {
-    request.setAttribute 'prev', page - 1
-}
-if (params.page != null) {
-    forward '/WEB-INF/pages/browse.gtpl'
+
+if (start <= stop) {
+    request.setAttribute 'imageList', imageList[start..stop]
+    request.setAttribute 'appUtil', AppUtil.instance
+    if (stop + 1 < imageList.size()) {
+        request.setAttribute 'next', page + 1
+    }
+    if (start > 0) {
+        request.setAttribute 'prev', page - 1
+    }
+    if (params.page != null) {
+        forward '/WEB-INF/pages/browse.gtpl'
+    } else {
+        forward '/WEB-INF/pages/browsePage.gtpl'
+    }
 } else {
-    forward '/WEB-INF/pages/browsePage.gtpl'
+    response.sendError(404)
 }
