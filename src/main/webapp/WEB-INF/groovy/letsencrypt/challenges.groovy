1385832be60962a27b397f829b2b3997835bbcfe
Rahul Somasunderam <rahul.som@gmail.com>
2017-11-27 21:19:33 -0800

    Remove letsencrypt code
    GAE offers its own certificate management now which doesn’t require any
    custom code.

--- src/main/webapp/WEB-INF/groovy/letsencrypt/challenges.groovy
+++
@@ -1,9 +0,0 @@
-import domain.letsencrypt.Challenge
-import util.AuthorizedUsers
-import util.GithubAuthUtil
-
-def githubAuthUtil = new GithubAuthUtil(request, response)
-githubAuthUtil.withValidUser('/banned', AuthorizedUsers.allowDelete) {
-    request.setAttribute 'challenges', Challenge.findAll()
-    forward('/WEB-INF/pages/letsencrypt/challenges.gtpl')
-}
--- src/main/webapp/WEB-INF/routes.groovy
+++ src/main/webapp/WEB-INF/routes.groovy
@@ -36,9 +36,5 @@ post "/mailUser",                                 forward: "/mailUser.groovy"
 get "/banned",                                    forward: "/banned.groovy"
 get "/unban/@username",                           forward: "/unban.groovy?username=@username"
 
-get "/letsencrypt/challenges",                    forward: "/letsencrypt/challenges.groovy"
-post "/letsencrypt/save",                         forward: "/letsencrypt/save.groovy"
-get "/letsencrypt/delete/@id",                    forward: "/letsencrypt/deleteChallenge.groovy?id=@id"
-get "/.well-known/acme-challenge/@challengeText", forward: "/letsencrypt/get.groovy?challengeText=@challengeText"
-
 get '/jwt/generate',                              forward: '/tokens/gentoken.groovy'
+
